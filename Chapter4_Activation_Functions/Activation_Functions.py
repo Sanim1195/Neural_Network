9df0e7d48e39d1e1c03cb34fa0761bc550252ac7
Sanim1195 <pokhrelsanim@gmail.com>
2024-11-01 22:38:28 +1300

    Activation Functions

---
+++ Chapter4_Activation_Functions/Activation_Functions.py
@@ -0,0 +1,7 @@
+#  We use  activation functions because if the activation function itself is "non-linear", it allows for neural networks with usually two or more hidden layers to map nonlinear functions
+
+#  your neural network will have two types of activation functions. The first will be the activation function used in hidden layers that can produce non linear outputs, and the second will be linear function used in the output layer.
+
+# Usually,the activation function used for hidden neurons will be the same for all of them, but it doesn’t have to.
+
+# Tweaing Bias and weigyhts chart:
